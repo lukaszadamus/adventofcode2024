3309d40b0cd5aca3b1b3e4277908a22539493858
lukaszadamus <lukasz.adamus@soodo.pl>
2024-12-03 21:39:01 +0100

    One regex to rule them...

--- day03/Program.cs
+++ day03/Program.cs
@@ -1,27 +1,59 @@
 ﻿using System.Text.RegularExpressions;
 
-var rxMul1 = new Regex(@"((?<mul>mul)[(]{1}(?<left>\d{1,3})[,]{1}(?<right>\d{1,3})[)]{1})", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-var result1 = rxMul1.Matches(File.ReadAllText("input.txt")).Select(x => int.Parse(x.Groups["left"].Value) * int.Parse(x.Groups["right"].Value)).Sum();
-Console.WriteLine(result1);
 
-var rxMul2 = new Regex(@"(?<do>do[(]{1}[)]{1})|(?<dont>don't[(]{1}[)]{1})|((?<mul>mul)[(]{1}(?<left>\d{1,3})[,]{1}(?<right>\d{1,3})[)]{1})", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-var count = true;
+var lexer = new Lexer();
+var tokens = lexer.Instructions(File.ReadAllText("input.txt"));
 
-var result2 = rxMul2.Matches(File.ReadAllText("input.txt")).Select(x =>
+var computer = new Computer(tokens);
+computer.Run();
+
+Console.WriteLine($"part1: {computer.DoRegistry + computer.DontRegistry}");
+Console.WriteLine($"part2: {computer.DoRegistry}");
+
+class Computer(MatchCollection instructions)
 {
-    var value = 0;
-    if (x.Value == "do()")
+    private readonly MatchCollection _instructions = instructions;
+    public long DoRegistry { get; private set; } = 0;
+    public long DontRegistry { get; private set; } = 0;
+    public bool Do { get; private set; } = true;
+
+    public void Run()
     {
-        count = true;
+        foreach (Match instruction in _instructions)
+        {
+            var operation = instruction.Groups["operation"].Value;
+
+            switch (operation)
+            {
+                case "do":
+                    Do = true;
+                    break;
+                case "don't":
+                    Do = false;
+                    break;
+                case "mul":
+                    var result = int.Parse(instruction.Groups["left"].Value) * int.Parse(instruction.Groups["right"].Value);
+                    if (Do)
+                    {
+                        DoRegistry += result;
+                    }
+                    else
+                    {
+                        DontRegistry += result;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-    else if (x.Value == "don't()")
-    {
-        count = false;
-    }
-    else
-    {
-        value = count ? int.Parse(x.Groups["left"].Value) * int.Parse(x.Groups["right"].Value) : 0;
-    }
-    return value;
-}).Sum();
-Console.WriteLine(result2);
+}
+
+partial class Lexer
+{
+    private readonly Regex _regex = MyRegex();
+    [GeneratedRegex(@"(?<operation>mul|do|don't)(\(\)|\((?<left>\d{1,3}),(?<right>\d{1,3})\))", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+    private static partial Regex MyRegex();
+    public MatchCollection Instructions(string input) => _regex.Matches(input);
+}
