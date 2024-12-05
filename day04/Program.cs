d7df994551aea2f32057c4589d47281c4957cec1
lukaszadamus <lukasz.adamus@soodo.pl>
2024-12-05 23:01:46 +0100

    Day 4&5

---
+++ day04/Program.cs
@@ -0,0 +1,114 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+// part 1
+
+var pattern1 = new char[] { 'X', 'M', 'A', 'S' };
+var revPattern1 = new char[] { 'S', 'A', 'M', 'X' };
+var toCheck1 = new List<char[]>();
+
+toCheck1.AddRange(GetRows(input, pattern1.Length));
+toCheck1.AddRange(GetCols(input, pattern1.Length));
+toCheck1.AddRange(GetDiagonals(input, pattern1.Length));
+
+Console.WriteLine($"Part1: {toCheck1.Where(x => x.SequenceEqual(pattern1) || x.SequenceEqual(revPattern1)).Count()}");
+
+// part 2
+
+var pattern2 = new char[]{'M', 'A', 'S'};
+var revPattern2 = new char[]{'S', 'A', 'M'};
+
+Console.WriteLine($"Part2: {GetSlices(input, pattern2.Length).Where(x => 
+(x.PrimaryDiagonal.SequenceEqual(pattern2) && x.SecondardyDiagonal.SequenceEqual(pattern2))
+|| (x.PrimaryDiagonal.SequenceEqual(revPattern2) && x.SecondardyDiagonal.SequenceEqual(revPattern2))
+|| (x.PrimaryDiagonal.SequenceEqual(pattern2) && x.SecondardyDiagonal.SequenceEqual(revPattern2))
+|| (x.PrimaryDiagonal.SequenceEqual(revPattern2) && x.SecondardyDiagonal.SequenceEqual(pattern2))
+).Count()}");
+
+static List<Slice> GetSlices(string[] input, int size)
+{
+    var slices = new List<Slice>();
+
+    for (var r = 0; r <= input.Length - size; r++)
+    {
+        for (var c = 0; c <= input[0].Length - size; c++)
+        {
+            slices.Add(GetSlice(input, r, c, size));
+        }
+    }
+    return slices;
+}
+
+static List<char[]> GetRows(string[] lines, int size)
+{
+    var rows = new List<char[]>();
+    for(var r=0; r<lines.Length; r++)
+    {
+        for(var c=0; c<=lines[r].Length - size; c++)
+        {
+            rows.Add(lines[r].Skip(c).Take(size).ToArray());
+        }
+    }
+    return rows;
+}
+
+static List<char[]> GetCols(string[] lines, int size)
+{
+    var cols = new List<char[]>();
+    for (var c = 0; c < lines[0].Length; c++)
+    {
+        for (var r = 0; r <= lines.Length - size; r++)
+        {
+            cols.Add(lines.Skip(r).Take(size).Select(x => x[c]).ToArray());
+        }
+    }
+    return cols;
+}
+
+static List<char[]> GetDiagonals(string[] lines, int size)
+{
+    
+    List<char[]> diagonals = [];
+
+    for (var r = 0; r <= lines.Length - size; r++)
+    {
+        for (var c = 0; c <= lines[r].Length - size; c++)
+        {
+            var pd = new List<char>();
+            var sd = new List<char>();
+            for (int i= 0, j = 0; i < size; i++, j++)
+            {
+                pd.Add(lines[r+i][c+j]);
+                sd.Add(lines[r+i][c+size - j - 1]);
+            }
+            diagonals.Add([.. pd]);
+            diagonals.Add([.. sd]);
+        }
+    }
+
+    return diagonals;
+}
+
+static Slice GetSlice(string[] lines, int row, int col, int size)
+{
+    var sliced = lines
+        .Skip(row)
+        .Take(size)
+        .Select(x => x.Skip(col).Take(size).ToList())
+        .Select(x => x)
+        .ToList();
+
+    var pd = new List<char>();
+    var sd = new List<char>();
+
+    for (int r = 0, c = 0; r < size; r++, c++)
+    {
+        pd.Add(sliced[r][c]);
+        sd.Add(sliced[r][size - c - 1]);
+    }
+
+    return new Slice(pd, sd);
+}
+
+record Slice(List<char> PrimaryDiagonal, List<char> SecondardyDiagonal);
+
+
---
+++ day05/Program.cs
@@ -0,0 +1,119 @@
+﻿var input = ParseInput();
+
+Console.WriteLine($"Part 1: {input.Updates.Where(x => IsInOrder(x, input.Rules)).Select(x => x[(x.Count - 1) / 2]).Sum()}");
+Console.WriteLine($"Part 2: {input.Updates.Where(x => !IsInOrder(x, input.Rules)).Select(x => FixOrder(x, input.Rules)).Select(x => x[(x.Count - 1) / 2]).Sum()}");
+
+static bool IsInOrder(List<int> update, Dictionary<int, PageRule> rules)
+{
+    for (var i = 0; i < update.Count; i++)
+    {
+        var pageRules = rules[update[i]];
+
+        var notInOrder = pageRules.After.Intersect(update.Skip(i + 1)).Any() || pageRules.Before.Intersect(update.Take(i)).Any();
+
+        if (notInOrder)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+static List<int> FixOrder(List<int> update, Dictionary<int, PageRule> rules)
+{
+    var newOrder = new List<int>();
+
+    foreach (var toInsert in update)
+    {
+        var index = GetIndex(newOrder.Count / 2, newOrder.Count / 2, toInsert, newOrder, rules);
+        if (index < newOrder.Count)
+        {
+            newOrder.Insert(index, toInsert);
+        }
+        else
+        {
+            newOrder.Add(toInsert);
+        }
+    }
+
+    return newOrder;
+}
+
+static int GetIndex(int index, int prevIndex, int page, List<int> pages, Dictionary<int, PageRule> rules)
+{
+    if (pages.Count == 0 || index < 0)
+    {
+        return 0;
+    }
+    else if (index >= pages.Count)
+    {
+        return pages.Count;
+    }
+    else
+    {
+        var indexRules = rules[pages[index]];
+
+        if (indexRules.After.Contains(page))
+        {
+            return prevIndex < index ? index : GetIndex(index - 1, index, page, pages, rules);
+        }
+        else if (indexRules.Before.Contains(page))
+        {
+            return prevIndex > index ? prevIndex : GetIndex(index + 1, index, page, pages, rules);
+        }
+
+        return index;
+    }
+}
+
+
+static Input ParseInput()
+{
+    var lines = File.ReadAllLines("input.txt");
+    var input = new Input([], []);
+    var parsingRules = true;
+    foreach (var line in lines)
+    {
+        if (line == "")
+        {
+            parsingRules = false;
+            continue;
+        }
+
+        if (parsingRules)
+        {
+            var pages = line.Split('|').Select(int.Parse);
+            var left = pages.First();
+            var right = pages.Last();
+
+            var rule = new PageRule([], []);
+
+            if (input.Rules.TryGetValue(left, out rule))
+            {
+                rule.Before.Add(right);
+            }
+            else
+            {
+                input.Rules.Add(left, new PageRule([], [right]));
+            }
+
+            if (input.Rules.TryGetValue(right, out rule))
+            {
+                rule.After.Add(left);
+            }
+            else
+            {
+                input.Rules.Add(right, new PageRule([left], []));
+            }
+        }
+        else
+        {
+            input.Updates.Add(line.Split(',').Select(int.Parse).ToList());
+        }
+    }
+
+    return input;
+}
+record Input(Dictionary<int, PageRule> Rules, List<List<int>> Updates);
+record PageRule(List<int> After, List<int> Before);
